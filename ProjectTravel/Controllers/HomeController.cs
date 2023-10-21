cf8ce0a3dc7388f68b66281ee72ef6b0997f1cea
STEIN <manhphuc2003@gmail.com>
2023-10-21 20:20:12 +0700

    Post Detail

--- ProjectTravel/Controllers/HomeController.cs
+++ ProjectTravel/Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace ProjectTravel.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -22,6 +24,22 @@ namespace ProjectTravel.Controllers
         {
             return View();
         }
+
+        [Route("/post-{slug}-{id:long}.html", Name = "Details")]
+        public IActionResult Details(long? id)
+        {
+            if (id == null) 
+            { 
+                return NotFound();
+            }
+            var post = _context.Posts.FirstOrDefault(m => (m.PostID == id) && (m.IsActive == true));
+            if (post == null) 
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
